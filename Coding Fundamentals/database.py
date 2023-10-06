9a05dc2f7d4a78ac60e5d5b86caeb16e46409ee2
Justin <you@hi.com>
2023-10-06 16:09:06 +0100

    hehehe

--- Coding Fundamentals/database.py
+++ Coding Fundamentals/database.py
@@ -2,19 +2,44 @@ import pyodbc
 
 connectionString = r'DRIVER={ODBC Driver 17 for SQL Server};SERVER=MIS;DATABASE=qastore;Trusted_Connection=yes'
 
-newtable = """
-CREATE TABLE Cars(
-    Manufacturer NVARCHAR(30) PRIMARY KEY,
-    Model NVARCHAR(30),
-    Year INT,
-    Engine size NVARCHAR(30),
-    Engine NVARCHAR(30)
-    );
-    """
+#newtable = """
+#CREATE TABLE Cars(
+#    Manufacturer NVARCHAR(30) PRIMARY KEY,
+#    Model NVARCHAR(30),
+#    Year INT,
+#    [Engine size] NVARCHAR(30),
+#    Engine NVARCHAR(30)
+#    );
+#    """
 
-conn = pyodbc.connect(connectionString)
-cur = conn.cursor()
-cur.execute(newtable)
 
-conn.commit()
-conn.close()
+
+try:
+    conn = pyodbc.connect(connectionString)
+    cur = conn.cursor()
+
+    insert_sql = """
+    INSERT INTO Cars  (Manufacturer, Model, Year, [Engine Size], Engine)
+    VALUES (?, ?, ?, ?, ?);"""
+    data = ('Audi', 'RS6', 2023, '4.0L', 'V8')
+
+#    read  = "SELECT * FROM Cars;"
+    cur.execute(insert_sql, data)
+
+# row = cur.fetchall()
+#  for r in row:
+#       print(r)
+
+    conn.commit()
+    conn.close()
+    print("data inserted")
+except pyodbc.Error as ex:
+    print("SQL Error:", ex)
+except Exception as w:
+    print("General Error:", w)
+finally:
+    try:
+        cur.close()
+        conn.close()
+    except:
+        pass
