66e80826214c30fb79ad956802b2335b7fcc52cb
Justin <you@hi.com>
2023-11-17 14:23:03 +0000

    terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,21 +4,54 @@ provider "aws" {
  region       = "eu-west-2"
 }
 
-resource "aws_instance" "demo1" {
- ami            = var.ami
- instance_type  = "t2.micro"
- key_name       = "mykey"
-}
+resource "aws_vpc" "example_vpc" {
+  cidr_block = var.vpc_cidr
+  enable_dns_support   = true
+  enable_dns_hostnames = true
 
-resource "aws_s3_bucket" "my_bucket" {
-  bucket = var.bucket
   tags = {
-    Name        = "My bucket"
-    Environment = "Dev"
+    Name = "example-vpc"
   }
 }
 
-resource "aws_security_group" "ssh_access" {
+resource "aws_subnet" "example_subnet" {
+  vpc_id     = aws_vpc.example_vpc.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "example-subnet"
+  }
+}
+
+resource "aws_internet_gateway" "example_igw" {
+  vpc_id = aws_vpc.example_vpc.id
+
+  tags = {
+    Name = "example-igw"
+  }
+}
+ 
+
+resource "aws_route_table" "example_rt" {
+  vpc_id = aws_vpc.example_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.example_igw.id
+  }
+
+  tags = {
+    Name = "example-rt"
+  }
+}
+
+resource "aws_route_table_association" "example_rta" {
+  subnet_id      = aws_subnet.example_subnet.id
+  route_table_id = aws_route_table.example_rt.id
+}
+
+resource "aws_security_group" "ssh_access_sg" {
+  vpc_id = aws_vpc.example_vpc.id
   name        = "ssh_access_sg"
   description = "Security group for SSH access"
 
@@ -38,5 +71,27 @@ resource "aws_security_group" "ssh_access" {
 
   tags = {
     Name = "ssh_access_sg"
+  }
+}
+
+
+
+resource "aws_instance" "demo1" {
+ ami              = var.ami
+ instance_type    = "t2.micro"
+ key_name         = "mykey"
+ subnet_id        = aws_subnet.example_subnet.id
+ security_groups  = [aws_security_group.ssh_access_sg.id]
+
+ tags = {
+  Name = "demo1"
+ }
+}
+
+resource "aws_s3_bucket" "my_bucket" {
+  bucket         = var.bucket
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
   }
 }
--- terraform/output.tf
+++ terraform/output.tf
@@ -1,7 +1,19 @@
 output "vm_public_ip" {
-    value = aws_instance.demo1.public_ip
+    value = aws_instance.demo1.id
 }
 
 output "s3_domain" {
     value = aws_s3_bucket.my_bucket.bucket_domain_name
+}
+
+output "vpc_id" {
+  value = aws_vpc.example_vpc.id
+}
+
+output "subnet_id" {
+  value = aws_subnet.example_subnet.id
+}
+
+output "sg_id" {
+    value = aws_security_group.ssh_access_sg.id
 }
